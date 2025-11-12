ae8dec19b3a3e44246ceb2f84944261f7290fcc8
eugenionet <e.rinella@bluewin.ch>
2025-11-13 00:09:30 +0100

    Update app.py

--- outliers-app-db-python-main/outliers-app-db-python-main/app.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/app.py
@@ -33,7 +33,7 @@ outliers, ozone = helpers.create_outliers_table(ozone, "PPM")
 # Need the entire outliers table
 outliers = outliers.to_pandas()
 
-ui.page_opts(fillable=True, title="Identify suspicious values in air quality data $\sum$ 23")
+ui.page_opts(fillable=True, title="Identify suspicious values in air quality data $$\sum$$ 3")
 
 with ui.layout_columns():
 
@@ -82,7 +82,7 @@ with ui.layout_columns():
             ui.card_header("About this app", class_="bg-light")
             
             ui.markdown(
-                """ $\sum$ This app uses ozone data from the [EPA](https://www.epa.gov/outdoor-air-quality-data). 
+                """ $\\sum$ This app uses ozone data from the [EPA](https://www.epa.gov/outdoor-air-quality-data). 
                 The values shown in blue represent rows where `PPM` (ozone level in parts-per-million) was an outlier, 
                 identified using the [IQR method](https://en.wikipedia.org/wiki/Interquartile_range#Outliers). 
                 Some of these values are real, but some are errors, created for the purposes of this app.  
