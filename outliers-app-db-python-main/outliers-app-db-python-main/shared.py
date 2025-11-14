fba7132099db4bbf8ae22256e6f42b6ff3c671bd
eugenionet <e.rinella@bluewin.ch>
2025-11-14 18:17:24 +0100

    Update shared.py

--- outliers-app-db-python-main/outliers-app-db-python-main/shared.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/shared.py
@@ -25,18 +25,6 @@ mathjax = ui.head_content(
         #src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
     ),
     ui.tags.script("if (window.MathJax) MathJax.Hub.Queue(['Typeset', MathJax.Hub]);"),
-    ui.tags.script(
-        MathJax.Hub.Config({
-            tex2jax: {
-                inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-                processEscapes: true
-            }
-        });
-    ),
-    ui.tags.script(
-        #src="https://mathjax.rstudio.com/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
-        src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
-    ),
 )
 
 
