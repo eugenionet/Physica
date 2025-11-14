8da836d9781a4d378fe51562e10f5ffd2baf3d25
eugenionet <e.rinella@bluewin.ch>
2025-11-14 18:05:05 +0100

    Update shared.py

--- outliers-app-db-python-main/outliers-app-db-python-main/shared.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/shared.py
@@ -20,15 +20,17 @@ def restrict_width(*args, sm=None, md=None, lg=None, pad_y=5, **kwargs):
 
 # Allow LaTeX to be displayed via MathJax
 mathjax = ui.head_content(
-        MathJax.Hub.Config({
-            tex2jax: {
-              inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-              processEscapes: true
-            }
-        });
     ui.tags.script(
         #src="https://mathjax.rstudio.com/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
         src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+    ),
+    ui.tags.script(
+        MathJax.Hub.Config({
+            tex2jax: {
+                inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+                processEscapes: true
+            }
+        });
     ),
     ui.tags.script("if (window.MathJax) MathJax.Hub.Queue(['Typeset', MathJax.Hub]);"),
 )
