2c4267293aceb10b81bf3a381faa2889c0b02d85
eugenionet <e.rinella@bluewin.ch>
2025-11-14 19:05:18 +0100

    Update app.py

--- outliers-app-db-python-main/outliers-app-db-python-main/app.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/app.py
@@ -92,7 +92,7 @@ with ui.layout_columns():
             ui.card_header("About this app \( \sqrt{3x-1}+(1+x)^2 \) ", class_="bg-light")
             
             ui.markdown(
-                "\\( \sqrt{3x-1}+(1+x)^2 \\)"
+                "\\\( \frac{\sqrt{3x-1}+(1+x)^2}{n} \\\)"
                 """ This sentence uses \\( delimiters to show math inline: \\\( \sqrt{3x-1}+(1+x)^2 \\\) --- This app uses ozone data from the [EPA](https://www.epa.gov/outdoor-air-quality-data). 
                 The values shown in blue represent rows where `PPM` (ozone level in parts-per-million) was an outlier, 
                 identified using the [IQR method](https://en.wikipedia.org/wiki/Interquartile_range#Outliers). 
