02577c17965b5228512915dfa8d5d2d05b4c55a7
eugenionet <e.rinella@bluewin.ch>
2025-11-15 02:04:16 +0100

    Update helpers.py

--- outliers-app-db-python-main/outliers-app-db-python-main/helpers.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/helpers.py
@@ -75,7 +75,7 @@ def plot_ozone(x, y, ozone, outliers):
 
 def create_editable_table(df):
     df["Date"] = df.Date.astype("string")
-    df["State"] = df.State.astype("string")
+    df["State"] = df.State.astype("markdown")
     return render.DataGrid(
         df,
         editable=True,
