56ddd7d9c36d19713e172e9d4acfcc1e75359050
eugenionet <e.rinella@bluewin.ch>
2025-11-16 18:48:32 +0100

    Update app.py

--- outliers-app-db-python-main/outliers-app-db-python-main/app.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/app.py
@@ -75,8 +75,8 @@ with ui.layout_columns():
     with ui.layout_column_wrap(width=1, heights_equal="row"):
         with ui.card():
             ui.card_header(
+            mathjax
                 ui.markdown(
-                    mathjax
                     f"{icon_svg('circle-info')} \\\(\sum\\\) Change `Flag` to `1` to flag a value as an error. Flagged points will appear red in the plot."
                 ),
                 class_="bg-light"
