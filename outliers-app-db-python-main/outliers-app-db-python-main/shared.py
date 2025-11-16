42e63ea981cc034626545f07d0eb8624ad0e647a
eugenionet <e.rinella@bluewin.ch>
2025-11-16 18:27:00 +0100

    Update shared.py

--- outliers-app-db-python-main/outliers-app-db-python-main/shared.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/shared.py
@@ -26,15 +26,6 @@ mathjax = ui.head_content(
         src="https://mathjax.rstudio.com/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML",
     ),
     ui.tags.script("if (window.MathJax) MathJax.Hub.Queue(['Typeset', MathJax.Hub]);"),
-        ui.tags.script("
-            MathJax = {
-            tex: {inlineMath: {'[+]': [['$', '$']]}},
-            svg: {fontCache: 'global'}
-          };
-          "),
-    ui.tags.script(
-        src="https://cdn.jsdelivr.net/npm/mathjax@4/tex-svg.js",
-    ),
 )
 
 prose = ui.markdown(open(app_dir / "prose.md").read())
