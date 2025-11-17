ccf60777f26ea201cc00d4e83c2e61364cb310dc
eugenionet <e.rinella@bluewin.ch>
2025-11-17 03:25:04 +0100

    Update shared.py

--- outliers-app-db-python-main/outliers-app-db-python-main/shared.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/shared.py
@@ -25,7 +25,7 @@ mathjax = ui.head_content(
         src="https://cdn.jsdelivr.net/npm/mathjax@4/tex-chtml.js",
         #src="https://mathjax.rstudio.com/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML",
     ),
-    #ui.tags.script("if (window.MathJax) MathJax.Hub.Queue(['Typeset', MathJax.Hub]);"),
+    ui.tags.script("if (window.MathJax) MathJax.Hub.Queue(['Typeset', MathJax.Hub]);"),
 )
 
 prose = ui.markdown(open(app_dir / "prose.md").read())
