943e548a059193acd7f6ce25d6a02f5c3596d567
eugenionet <e.rinella@bluewin.ch>
2025-11-17 03:58:29 +0100

    Update app.py

--- outliers-app-db-python-main/outliers-app-db-python-main/app.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/app.py
@@ -84,12 +84,11 @@ with ui.layout_columns():
                 class_="bg-light"
             )
 
-            ui.markdown(
+
             @render.data_frame
             def outliers_editable():
                 outliers["Date"] = outliers.Date.astype("string")
                 return helpers.create_editable_table(outliers)
-            )
                 
             ui.input_action_button("write_data", "Write to database", width="40%")
         
