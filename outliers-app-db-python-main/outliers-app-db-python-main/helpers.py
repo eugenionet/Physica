125924303606f9ab10802328077d01efbd562c44
eugenionet <e.rinella@bluewin.ch>
2025-11-20 00:58:46 +0100

    Update helpers.py

--- outliers-app-db-python-main/outliers-app-db-python-main/helpers.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/helpers.py
@@ -6,7 +6,7 @@ import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 
-import matplotlib
+#import matplotlib
 #matplotlib.use('TkAgg')
 
 #from shared import mathjax
@@ -99,6 +99,7 @@ def find_row_number(points, editable_table):
     df_inds = [flag_inds[i] for i in point_inds if i < len(flag_inds)]
     id = df.loc[df_inds, "ID"].values[0]
     return df_original[df_original["ID"] == id].index.values.astype(int)[0].item()
+
 
 
 
