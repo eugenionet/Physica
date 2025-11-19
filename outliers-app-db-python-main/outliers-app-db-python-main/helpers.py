ac8b0cebbd1cbc22fddb9e01f306c919fc8b878b
eugenionet <e.rinella@bluewin.ch>
2025-11-19 23:17:57 +0100

    Update helpers.py

--- outliers-app-db-python-main/outliers-app-db-python-main/helpers.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/helpers.py
@@ -79,7 +79,7 @@ def plot_ozone(x, y, ozone, outliers):
 def create_editable_table(df):
     df["Date"] = df.Date.astype("string")
     return render.DataGrid(
-        df,
+        matplotlib.df,
         editable=True,
         selection_mode="rows",
         summary=False,
@@ -98,6 +98,7 @@ def find_row_number(points, editable_table):
     df_inds = [flag_inds[i] for i in point_inds if i < len(flag_inds)]
     id = df.loc[df_inds, "ID"].values[0]
     return df_original[df_original["ID"] == id].index.values.astype(int)[0].item()
+
 
 
 
