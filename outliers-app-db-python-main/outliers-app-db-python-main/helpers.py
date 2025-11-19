93a716da5b6f0cf9e7bf9bed30db2013af70de69
eugenionet <e.rinella@bluewin.ch>
2025-11-20 00:55:58 +0100

    Update helpers.py

--- outliers-app-db-python-main/outliers-app-db-python-main/helpers.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/helpers.py
@@ -6,7 +6,7 @@ import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 
-#import matplotlib
+import matplotlib
 #matplotlib.use('TkAgg')
 
 #from shared import mathjax
@@ -79,7 +79,7 @@ def plot_ozone(x, y, ozone, outliers):
 
 def create_editable_table(df):
     df["Date"] = df.Date.astype("string")
-    return render.DataGrid(
+    return matplotlib.render.DataGrid(
         df,
         editable=True,
         selection_mode="rows",
@@ -99,6 +99,7 @@ def find_row_number(points, editable_table):
     df_inds = [flag_inds[i] for i in point_inds if i < len(flag_inds)]
     id = df.loc[df_inds, "ID"].values[0]
     return df_original[df_original["ID"] == id].index.values.astype(int)[0].item()
+
 
 
 
