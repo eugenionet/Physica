b7312eed3585c7567138a70db644be9d98baad05
eugenionet <e.rinella@bluewin.ch>
2025-11-21 00:03:35 +0100

    Update app.py

--- outliers-app-db-python-main/outliers-app-db-python-main/app.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/app.py
@@ -13,9 +13,6 @@ import pandas as pd
 #import seaborn as sns
 from shared import mathjax, prose, restrict_width
 
-#import matplotlib
-import markdown
-
 # Import MathJax for LaTeX rendering
 mathjax
 
@@ -163,6 +160,7 @@ def write_data():
         )
     else:
         ui.notification_show(ui.markdown("No changes to write to database."), type="warning")
+
 
 
 
