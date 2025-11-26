cd2d8608bb7b2747cc9e03618bc9d3cc752f47eb
eugenionet <e.rinella@bluewin.ch>
2025-11-26 22:48:01 +0100

    Update app.py

--- outliers-app-db-python-main/outliers-app-db-python-main/app.py
+++ outliers-app-db-python-main/outliers-app-db-python-main/app.py
@@ -15,23 +15,6 @@ from shared import mathjax, prose, restrict_width
 
 # Import MathJax for LaTeX rendering
 mathjax
-
-
-# Wrap HTML content in quotes and proper indentation
-html_content = """
-<script type="text/javascript" id="MathJax-script" async 
-        src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
-</script>
-<script>
-	MathJax = {
-	  tex: {
-		inlineMath: [['$', '$'], ['\\(', '\\)']]
-	  }
-	};
-</script>
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"> </script>
-"""
-
     
 
 # Explanation and Explore prose
@@ -178,20 +161,6 @@ def write_data():
         )
     else:
         ui.notification_show(ui.markdown("No changes to write to database."), type="warning")
-
-
-
-
-
-
-
-
-
-# Wrap HTML content in quotes and proper indentation
-html_content = """
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
- """
 
 
 
